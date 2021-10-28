41a49305f7275aa1284fc89de4aeec20ae2d0d7e
OladapoAO <Dapo.owolabi@outlook.com>
2021-10-28 17:16:16 -0400

    Initial Commit

---
+++ WebSplit/WebSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSplitApp.swift
+//  WebSplit
+//
+//  Created by Daps Owolabi on 28/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WebSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
